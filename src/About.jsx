abd328dc325097af1cded4fb17494f8ae8e47c09
Aaru-2111 <70098104+Aaru-2111@users.noreply.github.com>
2022-04-23 14:30:29 +0530

    ..

--- src/About.jsx
+++ src/About.jsx
@@ -1,16 +1,35 @@
+import axios from 'axios';
 import React, { Component } from 'react'
-
+import HelloWorldService from './api/todo/HelloWorldService';
 export default class About extends Component {
-  retrieveMessage=()=>{
-    console.log("this is retrieved message");
+  
+  constructor(props)
+ {   super(props)
+     this.state={
+     WelcomeMessage:'',
+    }
   }
+  retrieveMessage=()=>{
+    HelloWorldService.executeHelloWorldService()
+    .then (response=>this.handlesuccess(response) );
+    //catch()
+  }
+  handlesuccess=(response)=>{
+     this.setState({WelcomeMessage:response.data});
+  }
+  
   render() {
     return (
+      <>
       <div className="container">
         Click here to get customized message
         <button onClick={this.retrieveMessage} className="btn btn-success"> Get customized message</button>
 
       </div>
+       <div className="container">
+        {this.state.WelcomeMessage}
+        </div> 
+        </>
     )
   }
 }
