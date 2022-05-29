47a53a763d37fc913f55ee655c1f0bd120fe37c7
Aaru-2111 <70098104+Aaru-2111@users.noreply.github.com>
2022-05-29 22:19:29 +0530

    apis

--- src/About.jsx
+++ src/About.jsx
@@ -8,27 +8,56 @@ export default class About extends Component {
      this.state={
      WelcomeMessage:'',
     }
+    
   }
   retrieveMessage=()=>{
-    HelloWorldService.executeHelloWorldService()
-    .then (response=>this.handlesuccess(response) );
-    //catch()
+  //  HelloWorldService.executeHelloWorldService()
+  //  .then(response=>this.handlesuccess(response))
+  HelloWorldService.executebean()
+  .then(response=>this.handlesuccess(response))
+    // HelloWorldService.executevariablepath(this.props.)
+    // HelloWorldService.executeerror()
+    // .then (response =>this.handlesuccess(response) );     //handle success response 
+    // .catch(error => this.handleerror(error) )             //handle error response
+    
+    // .catch(error)=>this.handleerror(error) );
+    
+   
   }
-  handlesuccess=(response)=>{
-     this.setState({WelcomeMessage:response.data});
-  }
+handlesuccess=(response)=>{
+  console.log(response);
+  this.setState({WelcomeMessage:response.data.msg});
+}
+  //   //this.setState({WelcomeMessage:response.data}); for the normal string
+  //    this.setState({WelcomeMessage:response.data.msg});   //since it returns an object , see in the console its not just data it is msg inside the data
+
+  // }
+  // handleerror=(error)=>{
+  //   console.log(error.response);
+  //   //this.setState({error:response.data});
+  // }
+  // // getmessage=()=>{
+  //   HelloWorldService.executebean()
+  //   .then(response=>this.handlesuccess(response));
+  // }
   
   render() {
     return (
       <>
+      {/* <div>Welcome {this.props.About}
+        You can manage your todos <link to="/">here</link>
+      </div> */}
       <div className="container">
         Click here to get customized message
         <button onClick={this.retrieveMessage} className="btn btn-success"> Get customized message</button>
-
       </div>
-       <div className="container">
+      <div className="container">
         {this.state.WelcomeMessage}
-        </div> 
+      </div> 
+        {/* <button onClick={this.retrieveMessage} className="btn btn-danger">Click me!</button>
+        <div className="container">
+        {this.state.WelcomeMessage}
+        </div>  */}
         </>
     )
   }
--- src/api/todo/HelloWorldService.js
+++ src/api/todo/HelloWorldService.js
@@ -1,9 +1,24 @@
 import axios from "axios";
 class HelloWorldService{
+
   executeHelloWorldService(){
      return axios.get('http://localhost:8080/cute');
 //   console.log('executed service');
 }
+
+executebean(){
+  return axios.get('http://localhost:8080/cute-bean');
+}
+
+executevariablepath(name)
+{
+  return axios.get(`http://localhost:8080/cute-bean/path-variable/${name}`)
+}
+executeerror()
+{
+  return axios.get('http://localhost:8080/errormessage');
+}
+
 }
 export default new HelloWorldService()
 
